35d22b5b0cff8417b76a0326af2569ccdb6e65c7
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-05 16:21:05 -0400

    Messages

---
+++ app/src/main/java/com/example/chatproject/models/Message.java
@@ -0,0 +1,12 @@
+package com.example.chatproject.models;
+
+public class Message {
+    private String senderId;
+    private String receiverId;
+    private String message;
+    private long timestamp;
+
+    public Message() {}
+
+    // Getters and setters
+}
