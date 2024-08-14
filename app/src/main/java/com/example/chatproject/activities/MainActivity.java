1374e208cae001ea0d55f64d102e38d38e2e6854
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-13 22:02:45 -0400

    main activity

--- app/src/main/java/com/example/chatproject/activities/MainActivity.java
+++ app/src/main/java/com/example/chatproject/activities/MainActivity.java
@@ -131,12 +131,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.chatproject.R;
 import com.example.chatproject.activities.utilities.Constants;
 import com.example.chatproject.activities.utilities.PreferenceManager;
+import com.example.chatproject.adapters.RecentConversationsAdapter;
 import com.example.chatproject.databinding.ActivityMainBinding;
+import com.example.chatproject.models.ChatMessage;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private PreferenceManager preferenceManager;
+    private List<ChatMessage> conversations;
+    private RecentConversationsAdapter conversationsAdapter;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -145,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         preferenceManager = new PreferenceManager(getApplicationContext());
-
+        init();
         loadUserDetails();
 
         binding.buttonSignOut.setOnClickListener(new View.OnClickListener() {
@@ -155,12 +167,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.textName1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
+            startActivity(intent);
+        });
+
         binding.fabNewChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), UsersActivity.class));
             }
         });
+    }
+
+    private void init() {
+        conversations = new ArrayList<>();
+        conversationsAdapter = new RecentConversationsAdapter(conversations);
+        binding.conversationsRecyclerView.setAdapter(conversationsAdapter);
+        database = FirebaseFirestore.getInstance();
     }
 
     private void loadUserDetails() {
@@ -200,7 +224,50 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+
+
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+
+    private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
+      if(error != null) {
+          return;
+      }
+      if(value != null) {
+          for(DocumentChange documentChange : value.getDocumentChanges()) {
+              if(documentChange.getType() == DocumentChange.Type.ADDED) {
+                  String senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                  String receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                  ChatMessage chatMessage = new ChatMessage();
+                  chatMessage.senderId = senderId;
+                  chatMessage.receiverId = receiverId;
+                  if(preferenceManager.getString(Constants.KEY_USER_ID).equals(senderId)) {
+                      chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_RECEIVER_IMAGE);
+                      chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_RECEIVER_NAME);
+                      chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+
+                  }else {
+                      chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_SENDER_IMAGE);
+                      chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_SENDER_NAME);
+                      chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                  }
+
+                  chatMessage.message = documentChange.getDocument().getString(Constants.KEY_LAST_MESSAGE);
+                  chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                  conversations.add(chatMessage);
+              } else if (documentChange.getType() == DocumentChange.Type.MODIFIED) {
+                  for (int i = 0 ; i < conversations.size(); i++) {
+                      String senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                      String receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                      if (conversations.get(i).senderId.equals(senderId) && conversations.get(i).receiverId.equals(receiverId)) {
+                          conversations.get(i).message = documentChange.getDocument().getString(Constants.KEY_LAST_MESSAGE);
+                          conversations.get(i).dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                          break;
+                      }
+                  }
+              }
+          }
+      }
+    };
 }
